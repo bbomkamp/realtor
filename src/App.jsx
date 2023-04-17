c27a45b6b0f5b6ff461639966fe132eaf743002a
Brad Bomkamp <bradbomkamp@Brads-MacBook-Pro.local>
2023-04-17 12:59:57 -0500

    Added routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from '../pages/Home'
+import ForgotPassword from "../pages/ForgotPassword"
+import Offers from "../pages/Offers"
+import Profile from "../pages/Profile"
+import SignIn from "../pages/SignIn"
+import SignUp from "../pages/SignUp"
 
 
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+        <Route path="/" exact element={<Home/>}/>
+        <Route path="/forgot-password" exact element={<ForgotPassword/>}/>
+        <Route path="/offers" exact element={<Offers/>}/>
+        <Route path="/profile" exact element={<Profile/>}/>
+        <Route path="/sign-in" exact element={<SignIn/>}/>
+        <Route path="/sign-up" exact element={<SignUp/>}/>
+        </Routes>
+    </Router>
     </>
   )
 }
