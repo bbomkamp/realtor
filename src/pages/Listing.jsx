ac444f47fec2c653eeaa058017699f71bd8c3290
bbomkamp <bradbomkamp@gmail.com>
2023-04-25 09:44:50 -0500

    map added to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,8 +7,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
 import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa"
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 import Contact from '../components/Contact';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
 
@@ -110,26 +111,43 @@ export default function Listing() {
                             </li>
                         </ul>
                         {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
-                             <div className='mt-6'>
-                        <button onClick={()=>setContactLandlord(true)} 
-                        className='px-7 py-3 bg-blue-600 text-white font-medium 
+                            <div className='mt-6'>
+                                <button onClick={() => setContactLandlord(true)}
+                                    className='px-7 py-3 bg-blue-600 text-white font-medium 
                         text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg
                          focus:bg-blue-700 focus:shadow-lg w-full text-center transition 
                          duration-150 ease-in-out'>Contact Landlord</button>
 
-                        </div>
+                            </div>
                         )}
-                       {contactLandlord && (
-                        <Contact userRef={listing.userRef} listing={listing}/>
-                       )}
-                        
+                        {contactLandlord && (
+                            <Contact userRef={listing.userRef} listing={listing} />
+                        )}
+
                     </div>
                 </div>
 
 
-                <div className=' bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden'>
-
-                </div>
+                <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
             </div>
         </main>
     )
